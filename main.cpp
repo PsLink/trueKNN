a3f30d7a20f3b48c8e0e408bfc8808191a6cc406
PsLink <ps.pyluo@gmail.com>
2018-03-19 19:26:42 +0800

    single GPU version

--- main.cpp
+++ main.cpp
@@ -21,12 +21,17 @@ int main(int argc, char **argv) {
 	int batch_size = 10000000;
 	// int numOfData =  10000;
 	// int batch_size = 1000;
-	int numOfQuery = 1;
+	int numOfQuery = 250;
+	int queryFileID = 1;
 	int dim = 128;
-	char dataFile[100] = "/home/pingyi/DATASET/learn.bvecs";
-	char queryFile[100] = "/home/pingyi/DATASET/QUERY/query.csv";
+	int candidate_size = 1000;
 
-	FILE *fout = fopen("topk.query", "w");
+	char dataFile[100] = "/home/pingyi/DATASET/learn.bvecs";
+	char queryFile[100] = "/home/pingyi/DATASET/QUERY/query_1.csv";
+	char outputFile[100];
+
+	// FILE *fout = fopen("topk.query", "w");
+	FILE *fout;
 
 	// --------------------------------------------------------------------------
 	// initialization
@@ -45,10 +50,6 @@ int main(int argc, char **argv) {
 	std::vector<Record> ranking;
 	std::vector<Record> answers;
 
-	std::vector<int> groupSize;
-
-	groupSize.clear();
-	answers.clear();
 	query.resize(dim);
 
 	for (int qryID = 0; qryID < numOfQuery; qryID++){
@@ -58,11 +59,17 @@ int main(int argc, char **argv) {
 		for (int i=0; i<dim; i++)
 			query[i] = Queries[qryID][i];
 
+		answers.clear();
+
 		for (int batchID = 0; batchID < numOfData; batchID += batch_size) {
-			printf("batch %d.\n", batchID);
+			printf("batch %d.\n", batchID/batch_size);
+
 			distance.clear();
+			ranking.clear();
+
 			dis(query, DataSet, distance, batchID, batch_size);
 			// std::sort(distance.begin(),distance.end());
+
 			for (int i=0; i<batch_size; i++) {
 				Record tmp;
 				tmp.dist = sqrt(distance[i]);
@@ -72,23 +79,23 @@ int main(int argc, char **argv) {
 
 			std::sort(ranking.begin(),ranking.end(), &Compare);
 
-			for (int i=0; i<100; i++)
+			for (int i=0; i<=candidate_size; i++)
 				answers.push_back(ranking[i]);
-
-			//for (int i=0; i<batch_size; i++) {
-			//	printf("%d %0.5f \n", ranking[i].pID, ranking[i].dist);
-			//}
 		}
 
 		// record top k
+		strcpy(outputFile,"/home/pingyi/kNNs/query");
+		sprintf(outputFile, "%s_%d.csv", outputFile, qryID + queryFileID * 250);
+
+		fout = fopen(outputFile,"w");
+
 		std::sort(answers.begin(),answers.end(), &Compare);
-		fprintf(fout, "%d %.5f \n", answers[100].pID,answers[100].dist);
-		// fprintf(fout, "%f %f \n", answer[100],sqrt(answer[100]));
-		// fprintf(fout, "%f %f \n", answer[1000],sqrt(answer[1000]));
-		// fprintf(fout, "\n");
+		for (int i=0; i<=candidate_size; i++)
+			fprintf(fout, "%d %.5f \n", answers[i].pID,answers[i].dist);
+		fclose(fout);
+		answers.clear();
 	}
 
- 	fclose(fout);
 	return 0;
 }
 
